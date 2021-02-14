a38cf96c79ea8e87775c5f03eaf768e90c4c2162
syed-osama-hassan <syedosamahassan16@gmail.com>
2021-02-14 14:03:29 +0500

    creat components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {Header} from './components/Header'
+import {Balance} from './components/Balance'
+import "./App.css"
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 function App() {
   return (
-    <div className="App">
-      Hello World! thats it!
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <>
+            <h4>
+                Your Balance
+            </h4>
+            <h1>Rs0.00</h1>
+        </>
+    )
+}
